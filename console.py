6c391d17fe586e33a6eab6839d5af5a276033eac
Deard51 <raskovalovn10@gmail.com>
2025-11-23 22:26:22 +0500

    feat(console): implemented basic parameter parsing with safeguards

---
+++ console.py
@@ -0,0 +1,30 @@
+﻿import argparse
+from pathlib import Path
+
+# Initialize the parser
+parser = argparse.ArgumentParser(description='Calculate the average of a list of numbers.')
+
+# Add an argument
+parser.add_argument('path', type=Path, help='Absolute or relative path to the code file')
+parser.add_argument(
+    '--config',
+    type=Path,
+    default='Саша не отвечает по поводу конфига',
+    help='Absolute or relative path to the config file',
+)
+
+args = parser.parse_args()
+code_path, config_path = args.path, args.config
+
+
+def check_path(path: Path) -> bool:
+    if not path.exists() or not path.is_file():
+        print(f'{path} does not exist or is not a file')
+        return False
+    else:
+        return True
+
+
+if check_path(code_path) and check_path(config_path):
+    print('Code path:', code_path)
+    print('Config path:', config_path)
