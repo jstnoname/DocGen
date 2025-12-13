e0428299669d8559270a0392674a1edd347e6332
Alex <sas.spirin2006@gmail.com>
2025-12-13 20:04:19 +0500

    пока в работе

--- ai_requester.py
+++ ai_requester.py
@@ -1,4 +1,5 @@
-﻿from parser import Position, PosWithBody
+﻿import sys
+from parser import Position, PosWithBody
 from typing import NamedTuple
 
 from requests import post
@@ -20,8 +21,7 @@ SYS_INSTRUCTION = (
     "Параметр 'return' так же указывай через 'имя этого класса'/'имя этой функции или метода' к чему "
     "относиться, то есть должно получиться 'имя этого класса'/'имя этой функции или метода'/'return'. "
     "Каждое описание пиши на новой строке, не допускай пустых строк. Сам код писать не нужно, пиши "
-    "только документацию и строго следуй инструкциям. Если у объекта уже есть документация, то "
-    "пропускай, такие методы, функции или классы менять не нужно. Пиши документацию только на английском языке"
+    "только документацию и строго следуй инструкциям. Пиши документацию только на английском языке"
 )
 
 
@@ -48,7 +48,7 @@ class AIRequester:
         :param objects_to_doc: dict got from Parser
         """
         with open(".env", "r") as env:  # пока что будет так
-            apikey = env.readlines()[1].strip().split('=')[1]
+            apikey = env.readline().split('=')[1]
 
         self._url = f"{url}{model}:generateContent?key={apikey}"
         self._objects_to_doc = objects_to_doc
@@ -67,12 +67,7 @@ class AIRequester:
         docs: dict[str, PosWithDoc] = {}
 
         while not is_data_valid:
-            response = post(self._url, json=self._body, headers={"Content-Type": "application/json"})
-
-            if response.status_code != 200:
-                continue
-
-            documentations: str = response.json()["candidates"][0]["content"]["parts"][0]["text"]
+            documentations = self._request_to_get_doc()
             validated_docs = self._validate_data(documentations)
             if not validated_docs:
                 continue
@@ -83,42 +78,66 @@ class AIRequester:
 
         return docs
 
-    def _validate_data(self, documentations: str) -> dict[str, str] | None:
+    def _request_to_get_doc(self) -> str | None:
+        response = post(self._url, json=self._body, headers={"Content-Type": "application/json"})
+
+        if response.status_code == 429:
+            print(f"Please retry again after {response.json()["error"]["details"][-1]["retryDelay"]}")
+            sys.exit()
+
+        return None if response.status_code != 200 else response.json()["candidates"][0]["content"]["parts"][0]["text"]
+
+    def _validate_data(self, documentations: str | None) -> dict[str, str] | None:
         """
         validate documentations to check correctness
         :param documentations: doc from AI
         :return: None if data not valid, dict of object as key and doc this object as value
         """
-        result: dict[str, str] = {}
+        if documentations is None:
+            return None
 
-        for doc in documentations.split("\n"):
+        result: dict[str, str] = {}
+        docs = documentations.split("\n")
+
+        for doc in docs:
             if doc.strip() == "":
-                return None
-            object_name, object_doc = doc.split(":", 1)
+                continue
+            object_name, object_doc = doc.strip().split(":", 1)
 
             for key in self._objects_to_doc.keys():
                 if key.endswith(object_name):
                     result[key] = object_doc.strip()
                     break
-                elif key.endswith(object_name[: ((arg_index := object_name.rfind('/') + 1) - 1)]) and (
-                    object_name[arg_index:].startswith("param") or object_name[arg_index:].startswith("return")
-                ):
-                    result[key] += '\n:' + object_name[arg_index:] + ': ' + object_doc.strip()
-                    break
+                else:
+                    if "param" in object_name or "return" in object_name:
+                        object_name, arg = object_name.rsplit("/", 1)
+
+                        if key.endswith(object_name):
+                            result[key] += '\n:' + arg + ': ' + object_doc.strip()
+                            break
+                # elif key.endswith(object_name[: ((arg_index := object_name.rfind('/') + 1) - 1)]) and (
+                #     object_name[arg_index:].startswith("param") or object_name[arg_index:].startswith("return")
+                # ):
+                #     result[key] += '\n:' + object_name[arg_index:] + ': ' + object_doc.strip()
+                #     break
             else:
                 return None
-        return result
+        return result if len(result.keys()) == len(self._objects_to_doc.keys()) else None
 
     def _get_outer_objects_to_doc(self) -> list[str]:
         """
         get outer objects to doc to don't write double documentation
         :return: list of objects to doc
         """
-        values = [value for _, value in self._objects_to_doc.items()]
-        outer_objects: list[str] = ['\n'.join(values[0].body)]
+        outer_objects: list[str] = []
+        previous_key: str = ""
 
-        for i in range(len(self._objects_to_doc.values()) - 1):
-            if values[i].position.end_line < values[i + 1].position.start_line:
-                outer_objects.append('\n'.join(values[i + 1].body))
+        for key, value in self._objects_to_doc.items():
+            if previous_key == "":
+                outer_objects.append(''.join(value.body))
+                previous_key = key
+            elif not key.startswith(previous_key):
+                outer_objects.append(''.join(value.body))
+                previous_key = key
 
         return outer_objects
