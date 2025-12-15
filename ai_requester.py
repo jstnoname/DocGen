36cd45f11ef243692ec285a1cdfdd0f392dcb83f
Alex <sas.spirin2006@gmail.com>
2025-12-15 15:50:21 +0500

    Create abstract class BaseAIRequester

--- ai_requester.py
+++ ai_requester.py
@@ -1,7 +1,8 @@
-﻿from parser import Position, PosWithBody
+﻿from abc import ABC, abstractmethod
+from parser import Position, PosWithBody
 from typing import NamedTuple
 
-from requests import post
+from requests import RequestException, post
 
 SYS_INSTRUCTION = (
     "Ты помощник по программированию. Я буду давай тебе часть кода, а твоя задача проанализировать "
@@ -20,8 +21,7 @@ SYS_INSTRUCTION = (
     "Параметр 'return' так же указывай через 'имя этого класса'/'имя этой функции или метода' к чему "
     "относиться, то есть должно получиться 'имя этого класса'/'имя этой функции или метода'/'return'. "
     "Каждое описание пиши на новой строке, не допускай пустых строк. Сам код писать не нужно, пиши "
-    "только документацию и строго следуй инструкциям. Если у объекта уже есть документация, то "
-    "пропускай, такие методы, функции или классы менять не нужно. Пиши документацию только на английском языке"
+    "только документацию и строго следуй инструкциям. Пиши документацию только на английском языке"
 )
 
 
@@ -30,27 +30,23 @@ class PosWithDoc(NamedTuple):
     Documentation: str
 
 
-class AIRequester:
+class BaseAIRequester(ABC):
     """
-    AI Requester. Do requests to artificial intelligence and return documentation
-    At this moment will do requests only to Gemini
+    Base class for AIRequester. Inherit this class to create new requester to AI
     """
 
-    def __init__(
-        self,
-        objects_to_doc: dict[str, PosWithBody],
-        url: str = "https://weathered-truth-4ce8.alexspirin.workers.dev/v1/models/",
-        model: str = "gemini-2.5-flash",
-        apikey: str = "",
-    ):
+    def __init__(self, objects_to_doc: dict[str, PosWithBody], url: str, model: str, apikey: str):
         """
-        Init AIRequester.
-        :param objects_to_doc: dict got from Parser
+        Initialize BaseAIRequester.
+        :param objects_to_doc:
+        :param url:
+        :param model:
+        :param apikey:
         """
-        with open(".env", "r") as env:  # пока что будет так
-            apikey = env.readlines()[1].strip().split('=')[1]
+        self._url_to_ai = url
+        self._api_key_to_ai = apikey
+        self._model_of_ai = model
 
-        self._url = f"{url}{model}:generateContent?key={apikey}"
         self._objects_to_doc = objects_to_doc
         self._body = {
             "contents": [
@@ -59,66 +55,112 @@ class AIRequester:
             ]
         }
 
-    def get_docs(self) -> dict[str, PosWithDoc]:
-        return self._get_request_to_ai()
-
-    def _get_request_to_ai(self) -> dict[str, PosWithDoc]:
-        is_data_valid = False
-        docs: dict[str, PosWithDoc] = {}
-
-        while not is_data_valid:
-            response = post(self._url, json=self._body, headers={"Content-Type": "application/json"})
-
-            if response.status_code != 200:
-                continue
-
-            documentations: str = response.json()["candidates"][0]["content"]["parts"][0]["text"]
-            validated_docs = self._validate_data(documentations)
-            if not validated_docs:
-                continue
-
-            for key, value in validated_docs.items():
-                docs[key] = PosWithDoc(Position=self._objects_to_doc[key].position, Documentation=value)
-            is_data_valid = True
-
-        return docs
-
-    def _validate_data(self, documentations: str) -> dict[str, str] | None:
-        """
-        validate documentations to check correctness
-        :param documentations: doc from AI
-        :return: None if data not valid, dict of object as key and doc this object as value
-        """
-        result: dict[str, str] = {}
-
-        for doc in documentations.split("\n"):
-            if doc.strip() == "":
-                return None
-            object_name, object_doc = doc.split(":", 1)
-
-            for key in self._objects_to_doc.keys():
-                if key.endswith(object_name):
-                    result[key] = object_doc.strip()
-                    break
-                elif key.endswith(object_name[: ((arg_index := object_name.rfind('/') + 1) - 1)]) and (
-                    object_name[arg_index:].startswith("param") or object_name[arg_index:].startswith("return")
-                ):
-                    result[key] += '\n:' + object_name[arg_index:] + ': ' + object_doc.strip()
-                    break
-            else:
-                return None
-        return result
-
     def _get_outer_objects_to_doc(self) -> list[str]:
         """
         get outer objects to doc to don't write double documentation
         :return: list of objects to doc
         """
-        values = [value for _, value in self._objects_to_doc.items()]
-        outer_objects: list[str] = ['\n'.join(values[0].body)]
+        outer_objects: list[str] = []
+        previous_key: str = ""
 
-        for i in range(len(self._objects_to_doc.values()) - 1):
-            if values[i].position.end_line < values[i + 1].position.start_line:
-                outer_objects.append('\n'.join(values[i + 1].body))
+        for key, value in self._objects_to_doc.items():
+            if previous_key == "":
+                outer_objects.append(''.join(value.body))
+                previous_key = key
+            elif not key.startswith(previous_key):
+                outer_objects.append(''.join(value.body))
+                previous_key = key
 
         return outer_objects
+
+    def get_docs(self) -> dict[str, PosWithDoc]:
+        """
+        Get documentation for AIRequester
+        :return: dict, where key object to doc, value is doc
+        """
+        documentation: dict[str, PosWithDoc] = {}
+        count_of_tries = 0
+
+        while not documentation or count_of_tries < 3:
+            docs = self._get_docs_from_ai()
+            valid_docs = self._validate_docs(docs)
+            count_of_tries += 1
+
+            if valid_docs is not None:
+                documentation = valid_docs
+
+        if not documentation:
+            raise RequestException("Cannot get documentation. Please try again")
+
+        return documentation
+
+    @abstractmethod
+    def _get_docs_from_ai(self) -> str | None:
+        """
+        Get docs from AI
+        :return: str of docs or None if you couldn't get docs
+        """
+        pass
+
+    @abstractmethod
+    def _validate_docs(self, docs: str | None) -> dict[str, PosWithDoc] | None:
+        """
+        validate documentation for AIRequester
+        :param docs: docs from AI
+        :return: dict, where key object to doc, value is doc if docs is valid else None
+        """
+        pass
+
+
+class AIRequester(BaseAIRequester):
+    def __init__(
+        self,
+        objects_to_doc: dict[str, PosWithBody],
+        url: str = "https://weathered-truth-4ce8.alexspirin.workers.dev/v1/models/",
+        model: str = "gemini-2.5-flash",
+        apikey: str = "",
+    ):
+        with open(".env", "r") as env:  # пока что будет так
+            apikey = env.readline().split('=')[1]
+
+        super().__init__(objects_to_doc, url, model, apikey)
+
+        self._full_url_to_ai: str = f"{url}{model}:generateContent?key={apikey}"
+
+    def _validate_docs(self, docs: str | None) -> dict[str, PosWithDoc] | None:
+        if docs is None:
+            return None
+
+        result: dict[str, PosWithDoc] = {}
+        paths = sorted(list(self._objects_to_doc.keys()), key=list(self._objects_to_doc.keys()).index)
+
+        for doc in docs.split("\n"):
+            if doc.strip() == "" or ":" not in doc:
+                continue
+
+            object_name, object_doc = map(str.strip, doc.split(":", 1))
+
+            for object_path in paths:
+                if object_path.endswith(object_name):
+                    result[object_path] = PosWithDoc(self._objects_to_doc[object_path].position, object_doc)
+                    break
+                elif "param" in object_name or "return" in object_name:
+                    true_object_name, argument = object_name.rsplit('/', 1)
+
+                    if object_path.endswith(true_object_name):
+                        pos = result[object_path].Position
+                        doc = result[object_path].Documentation + f"\n:{argument}: {object_doc}"
+                        result[object_path] = PosWithDoc(pos, doc)
+                        break
+
+        return result if len(result.keys()) == len(paths) else None
+
+    def _get_docs_from_ai(self) -> str | None:
+        response = post(self._full_url_to_ai, json=self._body, headers={"Content-Type": "application/json"})
+
+        if response.status_code == 429:
+            raise RequestException(
+                f"To many requests. Please retry again after {response.json()["error"]["details"][-1]["retryDelay"]}"
+            )
+
+        return response.json()["candidates"][0]["content"]["parts"][0]["text"] if response.status_code == 200 else None
